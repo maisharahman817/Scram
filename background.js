c9da0efd075520d585716ca83ad9302394584a22
KarenH8 <kah10074@nyu.edu>
2025-07-08 21:44:43 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,11 +1,11 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (tab.url && tab.url.includes("indeed.com/jobs?")) {
         const queryParameters = tab.url.split("?")[1];
         const params = new URLSearchParams(queryParameters);
 
         chrome.tabs.sendMessage(tabId,{
             type: "NEW",
-            jobId: urlParameters.get("q"),
+            jobId: params.get("q"),
         });
     }
 });
