2475e96792a9203c9375cb62501b80afc6a5cdcf
kathryn habib <khabib@vassar.edu>
2025-07-23 14:10:10 -0400

    .

--- app.py
+++ app.py
@@ -26,6 +26,6 @@ def predict():
     })
 
 if __name__ == '__main__':
-    # Get the port from the environment variable (Render sets this automatically)
-    port = int(os.environ.get('PORT', 5001))  # Default to 5001 if PORT is not set
+    # Render will automatically set the PORT environment variable
+    port = int(os.environ.get('PORT', 5001))  # Default to 5001 if not set
     app.run(host='0.0.0.0', port=port)
